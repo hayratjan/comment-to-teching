faa6fd3c830c0c2d49e0ef6ff369ae4bbecd034d
hayratjan <1693305172@qq.com>
2022-04-02 19:43:28 +0800

    Changes

--- jiaoxuepingjia/settings.py
+++ jiaoxuepingjia/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 from django.core import mail
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -32,6 +33,7 @@ X_FRAME_OPTIONS = 'ALLOWALL'
 # Application definition
 
 INSTALLED_APPS = [
+    #'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
     'students',
     'teachers',
     'myadmin',
+
 ]
 
 MIDDLEWARE = [
@@ -60,7 +63,7 @@ ROOT_URLCONF = 'jiaoxuepingjia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -86,7 +89,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': 3306,
     }
 }
 
@@ -130,6 +133,47 @@ USE_TZ = False
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static'), ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# 隐藏右侧SimpleUI广告链接和使用分析
+SIMPLEUI_HOME_INFO = False
+SIMPLEUI_ANALYSIS = False
+SIMPLEUI_CONFIG = {
+    'system_keep': False,
+    'menu_display': ['学生管理', '教师管理', '课程管理', '调查卷管理', '评价管理', '管理员'],  # 开启排序和过滤功能, 不填此字段为默认排序和全部显示, 空列表[] 为全部不显示.
+    'dynamic': True,  # 设置是否开启动态菜单, 默认为False. 如果开启, 则会在每次用户登陆时动态展示菜单内容
+    'menus': [{
+        'name': '学生管理',
+        # 'icon': 'fas fa-code',
+        'url': '/admin/login/students/'
+    }, {
+        'name': '教师管理',
+        # 'icon': 'fas fa-code',
+        'url': '/admin/login/kecheng/'
+    }, {
+        'name': '调查卷管理',
+        # 'icon': 'fas fa-code',
+        'url': '/admin/login/tiku_1/'
+    }, {
+        'name': '评价管理',
+        'models': [{
+            'name': '首页',
+            'url': '/myadmin/'
+        }, {
+            'name': '评价成绩',
+            'url': '/myadmin/pingjia/'
+        }, {
+            'name': '班级评价率',
+            'url': '/myadmin/pingjia_pjl/'
+        }, {
+            'name': '未评价学生名单',
+            'url': '/myadmin/pingjia_not/'
+        }]
+    },
+        {
+            'name': '管理员',
+            'icon': 'fa fa-user',
+            'url': 'auth/user/'
+        }]
+}
--- jiaoxuepingjia/urls.py
+++ jiaoxuepingjia/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # django后台管理
@@ -24,3 +27,9 @@ urlpatterns = [
     path('myadmin/', include('myadmin.urls')),  # 后台
 
 ]
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
